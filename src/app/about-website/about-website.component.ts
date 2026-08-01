993f173a30045b79d106b9b40e86250d860ba27b
zhucy9974 <75172940+zhucy9974@users.noreply.github.com>
2026-08-01 23:22:13 +0200

    Modernize CV and standalone form components

--- src/app/about-website/about-website.component.ts
+++ src/app/about-website/about-website.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { NavbarService } from '../navbar/navbar.service';
+import {SharedModule} from 'src/app/shared/shared.module';
+import {TranslateModule} from '@ngx-translate/core';
 
 @Component({
-  standalone: false,
+  standalone: true,
+  imports: [TranslateModule],
   selector: 'app-about-website',
   templateUrl: './about-website.component.html',
-  styleUrls: ['./about-website.component.scss']
+  styleUrls: ['./about-website.component.scss'],
 })
 export class AboutWebsiteComponent implements OnInit {
-
-  constructor(private navbarService: NavbarService) { }
+  constructor(private navbarService: NavbarService) {}
 
   ngOnInit(): void {
     this.navbarService.sendCurrentItem('aboutWebsite');
   }
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarModule } from "./navbar/navbar.module";
 import { APP_INITIALIZER, LOCALE_ID, NgModule } from '@angular/core';
-import { LoginModule } from "./login/login.module";
-import { AboutWebsiteModule } from './about-website/about-website.module';
-import { AboutMeModule } from './about-me/about-me.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DemosModule } from './demos/demos.module';
 import { AppConfigService } from './app.config.service';
@@ -25,10 +22,7 @@ export function initializeApp(appConfigService: AppConfigService) {
     BrowserAnimationsModule,
     AppRoutingModule,
     DemosModule,
-    NavbarModule,
-    LoginModule,
-    AboutWebsiteModule,
-    AboutMeModule
+    NavbarModule
   ],
   providers: [AppConfigService, { provide: APP_INITIALIZER, useFactory: initializeApp, deps: [AppConfigService], multi: true }],
   bootstrap: [AppComponent]
